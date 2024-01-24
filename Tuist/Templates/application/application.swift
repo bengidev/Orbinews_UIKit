965ccd6f3488987629ea570b530ceaf06b3d7649
B e n g i <bambang.trd17@gmail.com>
2024-01-24 16:00:02 +0700

    refactor: add required parameters for initialize

--- Tuist/Templates/application/application.swift
+++ Tuist/Templates/application/application.swift
@@ -1,25 +1,38 @@
+import Foundation
 import ProjectDescription
 
+let companyName = "Anonymous"
+var defaultYear: String {
+  let dateFormatter = DateFormatter()
+  dateFormatter.dateFormat = "yyyy"
+  return dateFormatter.string(from: Date())
+}
+
+var defaultDate: String {
+  let dateFormatter = DateFormatter()
+  dateFormatter.dateFormat = "dd/MM/yyyy"
+  return dateFormatter.string(from: Date())
+}
+
 let nameAttribute: Template.Attribute = .required("name")
+let authorAttribute: Template.Attribute = .required("author")
+let yearAttribute: Template.Attribute = .optional("year", default: defaultYear)
+let dateAttribute: Template.Attribute = .optional("date", default: defaultDate)
+let companyAttribute: Template.Attribute = .optional("company", default: companyName)
+
 let projectPath = "."
 let appPath = "Application/"
 
 let template = Template(
   description: "Application template",
   attributes: [
-    nameAttribute
+    nameAttribute,
+    authorAttribute,
+    yearAttribute,
+    dateAttribute,
+    companyAttribute
   ],
   items: [
-    .file(
-      path: projectPath + "/Project.swift",
-      templatePath: "Project.stencil"
-    ),
-
-    .file(
-      path: projectPath + "/Package.swift",
-      templatePath: "Package.stencil"
-    ),
-
     .file(
       path: appPath + "/Sources/AppDelegate.swift",
       templatePath: "AppDelegate.stencil"
